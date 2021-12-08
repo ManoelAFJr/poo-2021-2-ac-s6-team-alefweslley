c3f67bf05dd67ecc458af7a1a3d6f044edfc6fd0
Francisco Weslley Silva de Souza <44758448+weslleycz@users.noreply.github.com>
2021-12-08 17:45:07 -0300

    Add files via upload

---
+++ BMExercio10.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Weslley
+ */
+public class BMExercio10 {
+     public static void main(String[] args){
+       int x=10;
+       int z=0;
+        do {  
+              System.out.println(x);  
+              z=z+1;  
+           }while(z <= 19);  
+    }  
+    }
